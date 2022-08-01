ec522f0b4ced90e27e8fc50687c294b7cfee261a
Gabriel Coelho da Cunha <43345507+gabrielcoelhodacunha@users.noreply.github.com>
2022-08-01 20:57:38 -0300

    Added CORS to Express

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import env from '../config';
 import database from '../database';
 import createRouters from './routes';
 
 database.initialize().then(() => {
 	const app = express();
+	app.use(cors());
 	app.use(express.urlencoded({ extended: false }));
 	app.use(express.json());
 	app.use(createRouters());
